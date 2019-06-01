c47d22a3a8a413a287927479ccbb7ce64897387f
bathulamanohar <bmanuyadav@gmail.com>
2019-06-01 13:01:00 +0530

    first commit

---
+++ demo_01_06/src/main/java/com/hcl/demo_01_06/Demo0106Application.java
@@ -0,0 +1,13 @@
+package com.hcl.demo_01_06;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo0106Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo0106Application.class, args);
+	}
+
+}
---
+++ demo_01_06/src/test/java/com/hcl/demo_01_06/Demo0106ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hcl.demo_01_06;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Demo0106ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
